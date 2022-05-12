8a769a89bd66118acfdac72d8950c6085a5d64e3
abrakib942 <abrakib942@gmail.com>
2022-05-12 18:25:13 +0600

    database connected and get service

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zro52.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("doctors_portal").collection("service");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello from Doctors Portal");
+});
+
+app.listen(port, () => {
+  console.log("Doctors App listening on port", port);
+});
